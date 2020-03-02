8b6da6733d1b6b4c4a1d9ed199828f2e427adc0b
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-01 22:32:07 -0700

    Add CSS filters

---
+++ src/filters.ts
@@ -0,0 +1,12 @@
+const filters: string[] = [
+  "contrast(2)",
+  "brightness(1.5)",
+  "invert(0.5)",
+  "hue-rotate(90deg)",
+  "hue-rotate(270deg)",
+  "saturate(2)",
+  "sepia(1)",
+  "grayscale(1)",
+];
+
+export default filters;
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,34 @@
 import "webextension-polyfill";
 
 import images from './images';
+import filters from './filters';
+
+const getFilters = (offset: number) => {
+  return Math.random() > 0.7 ? "filter: " + filters[offset % filters.length] + ";" : "";
+};
 
 const update = () => {
   const elements = document.getElementsByTagName("img");
 
-  const startOffset: number = Math.round(Math.random() * images.length);
+  const imageOffset: number = Math.round(Math.random() * images.length);
+  const filterOffset: number = Math.round(Math.random() * images.length);
 
-  console.log("Offset: " + startOffset);
+  console.log("Offset: " + imageOffset);
 
-  const likelihood = 0.5;
+  const likelihood = 1; //TODO: compute likelihood based on time of day
 
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].hasAttribute("yawning") || Math.random() > likelihood) continue;
-    elements[i].src = images[(startOffset + i) % images.length];
-    elements[i].setAttribute("style", "object-fit: cover;");
-    elements[i].setAttribute("yawning", "true");
+    let el = elements[i];
+    if (el.hasAttribute("yawning")) continue;
+    if (Math.random() < likelihood) {
+      el.src = images[(imageOffset + i) % images.length];
+      el.setAttribute("style", "object-fit: cover;" + getFilters(filterOffset + i));
+    }
+    el.setAttribute("yawning", "true");
   }
 };
 
-const observer = new MutationObserver((mutationsList, observer) => {
-  update();
-});
+const observer = new MutationObserver((_, __) => update());
 
 observer.observe(document.getRootNode(), {
   childList: true,
